457c61b4c15ba3b70611de86a004351af671c593
Uacias <filgum0326@gmail.com>
2024-03-30 13:13:38 +0100

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/webhook', async (req, res) => {
+    const {body} = req;
+    try {
+        console.log(body);
+    } catch(e) {
+        console.error(e);
+        return res.status(400).json();
+    }
+    return res.status(200).json();
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
