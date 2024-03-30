7bcd7d2b7a31fe5afdcd3013d80312a937af39e1
Uacias <filgum0326@gmail.com>
2024-03-30 17:11:17 +0100

    modify abi, partially working

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/webhook', async (req, res) => {
-    try {
-        console.log("req.body",req.body);
+app.post("/webhook", (req, res) => {
+  const webhook = req.body;
+  console.log("body", webhook);
 
-    } catch(e) {
-        console.error(e);
-        return res.status(400).json();
-    }
-    return res.status(200).json();
+  const transfers = webhook["erc20Transfers"];
+  console.log("transfers", transfers);
+
+  for (const transfer of transfers) {
+    const { from, to, tokenSymbol, valueWithDecimals, triggers } = transfer;
+    console.log("========================");
+    console.log("from", from),
+      console.log("to", to),
+      console.log("tokenSymbol", tokenSymbol),
+      console.log("value", valueWithDecimals),
+      console.log("triggers", triggers);
+  }
+
+  // for (const transfer of transfers) {
+  //     const { from, to, value, triggers } = transfer;
+  //     const senderAccount = from;
+  //     const receiverAccount = to;
+  //     const amount = value;
+  //     let formattedVal = "$" + amount.slice(0, -6) + "." + amount.slice(-6);
+
+  //     const leftBalance = triggers[0]['value'];
+  //     let formattedNum = "$" + leftBalance.slice(0, -6) + "." + leftBalance.slice(-6);
+
+  //     console.log(`================================================`)
+  //     console.log(`Account: ${senderAccount}`);
+  //     console.log(`Sent to: ${receiverAccount}`);
+  //     console.log(`Sent: ${formattedVal}`);
+  //     console.log(`Left Balance: ${formattedNum}`);
+  //     console.log(`================================================`)
+
+  // }
+  return res.status(200).json();
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Listening to streams`);
+});
--- stream.js
+++ stream.js
@@ -6,6 +6,31 @@ Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 });
 
+const abi = {
+  inputs: [{ internalType: "address", name: "", type: "address" }],
+  name: "balanceOf",
+  outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+  stateMutability: "view",
+  type: "function",
+};
+
+const triggerFrom = {
+  contractAddress: "$contract",
+  functionAbi: abi,
+  inputs: ["$from"],
+  type: "erc20transfer",
+};
+
+const triggerTo = {
+  contractAddress: "$contract",
+  functionAbi: abi,
+  inputs: ["$to"],
+  type: "erc20transfer",
+};
+
+
+const triggers = [triggerFrom, triggerTo];
+
 async function streams() {
   const newStream = await Moralis.Streams.add({
     chains: [EvmChain.SEPOLIA],
@@ -13,16 +38,20 @@ async function streams() {
     tag: "transfers",
     includeContractLogs: true,
     includeNativeTxs: false,
-    webhookUrl: "https://5168-83-6-174-22.ngrok-free.app/webhook",
-
+    webhookUrl: "https://6f51-83-6-174-22.ngrok-free.app/webhook",
   });
 
   const { id } = newStream.toJSON();
 
   const address = "0xa3EA94756a6d1f6Bc4727a38fe5F7aa4d568D52E";
   await Moralis.Streams.addAddress({ address, id });
-//   const tokenAddressUSDC = "0xfff9976782d46cc05630d1f6ebab18b2324d6b14";
-//   await Moralis.Streams.addAddress({ address: tokenAddressUSDC, id });
+  await Moralis.Streams.update({
+    id: id,
+    chains: [EvmChain.SEPOLIA],
+    triggers: triggers,
+});
+
+
 
   console.log("Stream created...");
 }
